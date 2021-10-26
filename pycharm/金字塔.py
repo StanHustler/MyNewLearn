6d6193336bf52867f7feb75b28b0a9372ee2609c
Stan Hustler <cf260469080@sina.com>
2021-10-26 10:32:04 +0800

    BYE BYE ONEDRIVE!

--- pycharm/金字塔.py
+++ pycharm/金字塔.py
@@ -9,12 +9,29 @@ Desc    :
 """
 
 
-n=int(input())+1
-for i in range(1,n):
+# n=int(input())+1
+# for i in range(1,n):
+#
+#     for j in range(n-i):
+#         print(" ",end='')
+#     for j in range(2*i - 1):
+#         print('*',end='')
+#
+#     print()
 
-    for j in range(n-i):
-        print(" ",end='')
-    for j in range(2*i - 1):
-        print('*',end='')
-
+n = eval(input())
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end='')
     print()
+
+# 倒序
+# x = eval(input())
+# i = 1
+# while i <= x:
+#     j = x # j=每行的*数
+#     while j >= i:
+#         print("*",end = "")
+#         j -= 1
+#     print()
+#     i += 1
