db5950310d0622837d46d7b9d3d38f20466da176
StanHustler <cf260469080@sina.com>
2021-12-27 10:57:13 +0800

    New: requests

---
+++ pycharm/Junior/爬虫1.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "connect error"
+
+
+if __name__ == '__main__':
+    # print(getHTMLText("http://wlacm.com"))
+
+    head="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62"
+    payload={"key1":"value1"}
+    r = requests.post("https://httpbin.org/post", params=payload,headers=head)
+    print(r.text)
