b0054ceda7bd444f924c43b5c3fefb4d2190d826
StanHustler <cf260469080@sina.com>
2021-12-27 13:43:02 +0800

    Mod: fix the header

--- pycharm/Junior/爬虫1.py
+++ pycharm/Junior/爬虫1.py
@@ -14,7 +14,6 @@ def getHTMLText(url):
 if __name__ == '__main__':
     # print(getHTMLText("http://wlacm.com"))
 
-    head="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62"
     payload={"key1":"value1"}
-    r = requests.post("https://httpbin.org/post", params=payload,headers=head)
+    r = requests.post("https://httpbin.org/post", params=payload,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'})
     print(r.text)
